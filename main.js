43be7440dfa6cc720e7aaa812922acf32d57973b
Kwangsoo, Seo <chps74@naver.com>
2018-10-15 18:38:58 +0900

    Update package.json, main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-﻿const { app, BrowserWindow } = require('electron')
+﻿const { app, BrowserWindow } = require('electron');
 const log = require('electron-log');
 const {autoUpdater} = require("electron-updater");
 
@@ -8,6 +8,9 @@ const isDevelopment = process.env.NODE_ENV === 'dev'
 
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
+autoUpdater.setFeedUrl({ token: '8b33e418321b07d19331e2ee15e0dfe156beb601',
+owner: 'monoslab',
+repo: 'testapp'});
 log.info('App starting...');
 
   // Keep a global reference of the window object, if you don't, the window will
@@ -17,10 +20,10 @@ log.info('App starting...');
   function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({ width: 1000, height: 600 })
-  
+
     // and load the index.html of the app.
     win.loadFile('index.html')
-  
+
     // Open the DevTools.
 
     if(isDevelopment){
@@ -41,12 +44,12 @@ log.info('App starting...');
       win = null
     })
   }
-  
+
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
   app.on('ready', createWindow)
-  
+
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
@@ -55,7 +58,7 @@ log.info('App starting...');
       app.quit()
     }
   })
-  
+
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
@@ -63,7 +66,7 @@ log.info('App starting...');
       createWindow()
     }
   })
-  
+
   // In this file you can include the rest of your app's specific main process
   // code. You can also put them in separate files and require them here.
 
@@ -97,4 +100,3 @@ log.info('App starting...');
   autoUpdater.on('update-downloaded', (info) => {
     sendStatusToWindow('Update downloaded');
   });
-
